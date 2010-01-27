7557b3a15c841d445d47d9e470333c51a513005d
B.Thax.DCG <B.Thax.DCG@844bf9d6-0849-11df-ae69-07492475f941>
2010-01-27 21:29:14 +0000

    - R.java update

--- trunk/src/meneameApp/gen/com/dcg/meneame/R.java
+++ trunk/src/meneameApp/gen/com/dcg/meneame/R.java
@@ -21,12 +21,12 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int Color_black=0x7f090003;
-        public static final int Color_dark_blue=0x7f090005;
-        public static final int Color_dark_grey=0x7f090001;
-        public static final int Color_grey=0x7f090000;
-        public static final int Color_orange=0x7f090004;
-        public static final int Color_white=0x7f090002;
+        public static final int Color_black=0x7f070003;
+        public static final int Color_dark_blue=0x7f070005;
+        public static final int Color_dark_grey=0x7f070001;
+        public static final int Color_grey=0x7f070000;
+        public static final int Color_orange=0x7f070004;
+        public static final int Color_white=0x7f070002;
     }
     public static final class drawable {
         public static final int ic_menu_refresh=0x7f020000;
@@ -34,6 +34,11 @@ public final class R {
         public static final int logo=0x7f020002;
         public static final int mnm_background=0x7f020003;
         public static final int orange=0x7f020004;
+        public static final int tab_focus=0x7f020005;
+        public static final int tab_indicator=0x7f020006;
+        public static final int tab_press=0x7f020007;
+        public static final int tab_selected=0x7f020008;
+        public static final int tab_unselected=0x7f020009;
     }
     public static final class id {
         public static final int applogo=0x7f0a0000;
@@ -51,7 +56,6 @@ public final class R {
         public static final int meneo_listitem=0x7f030002;
     }
     public static final class string {
-<<<<<<< .mine
         public static final int app_name=0x7f08000a;
         public static final int app_pref_name=0x7f080003;
         public static final int empty_list=0x7f08000e;
@@ -72,37 +76,19 @@ public final class R {
         public static final int pref_app=0x7f080002;
         public static final int pref_app_mainanimation=0x7f080001;
         public static final int pref_app_mainanimation_summary=0x7f080000;
-=======
-        public static final int app_name=0x7f07000a;
-        public static final int app_pref_name=0x7f070003;
-        public static final int empty_list=0x7f07000e;
-        public static final int hello=0x7f070009;
-        public static final int main_menu_refresh=0x7f070011;
-        public static final int main_menu_settings=0x7f070012;
-        public static final int main_tab_comments=0x7f07000d;
-        public static final int main_tab_news=0x7f07000b;
-        public static final int main_tab_queue=0x7f07000c;
-        public static final int meneo_item_open=0x7f07000f;
-        public static final int meneo_item_vote=0x7f070010;
-        public static final int pref_account=0x7f070008;
-        public static final int pref_account_apikey=0x7f070005;
-        public static final int pref_account_apikey_summary=0x7f070004;
-        public static final int pref_account_user=0x7f070007;
-        public static final int pref_account_user_summary=0x7f070006;
-        public static final int pref_app=0x7f070002;
-        public static final int pref_app_mainanimation=0x7f070001;
-        public static final int pref_app_mainanimation_summary=0x7f070000;
->>>>>>> .r15
     }
     public static final class style {
-        public static final int Style_app_logo=0x7f080004;
-        public static final int Style_grey_bg=0x7f080006;
-        public static final int Style_infolabel=0x7f080002;
-        public static final int Style_infolabel_orange=0x7f080003;
-        public static final int Style_orange_bg=0x7f080005;
-        public static final int Style_white_bg=0x7f080007;
-        public static final int Theme_mnm=0x7f080000;
-        public static final int Theme_mnm_prefs=0x7f080001;
+        public static final int MNM_Prefs_TextView_ListSeparator=0x7f090006;
+        public static final int MNM_TextAppearance_Widget_TabWidget=0x7f090008;
+        public static final int MNM_Widget_TabWidget=0x7f090007;
+        public static final int Style_app_logo=0x7f090002;
+        public static final int Style_grey_bg=0x7f090004;
+        public static final int Style_infolabel=0x7f090000;
+        public static final int Style_infolabel_orange=0x7f090001;
+        public static final int Style_orange_bg=0x7f090003;
+        public static final int Style_white_bg=0x7f090005;
+        public static final int Theme_mnm=0x7f090009;
+        public static final int Theme_mnm_prefs=0x7f09000a;
     }
     public static final class xml {
         public static final int preferences=0x7f050000;
