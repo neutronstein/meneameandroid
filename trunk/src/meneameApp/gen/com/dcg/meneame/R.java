e40d753b4fa28266d4cfd35822e06ffee07af3de
B.Thax.DCG <B.Thax.DCG@844bf9d6-0849-11df-ae69-07492475f941>
2010-01-30 12:10:33 +0000

    - added preferences, you need to uninstall the app completely! Because I had an key mismatch!

--- trunk/src/meneameApp/gen/com/dcg/meneame/R.java
+++ trunk/src/meneameApp/gen/com/dcg/meneame/R.java
@@ -15,8 +15,10 @@ public final class R {
         public static final int slide_right=0x7f040003;
     }
     public static final class array {
-        public static final int animationListEntries=0x7f060000;
-        public static final int animationListValues=0x7f060001;
+        public static final int pref_app_storage_entries=0x7f060000;
+        public static final int pref_app_storage_values=0x7f060001;
+        public static final int pref_style_mainanimation_entries=0x7f060002;
+        public static final int pref_style_mainanimation_values=0x7f060003;
     }
     public static final class attr {
     }
@@ -56,29 +58,62 @@ public final class R {
         public static final int meneo_listitem=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f08000d;
-        public static final int app_pref_name=0x7f080006;
-        public static final int empty_list=0x7f080011;
-        public static final int hello=0x7f08000c;
-        public static final int main_menu_notame=0x7f080015;
-        public static final int main_menu_refresh=0x7f080014;
-        public static final int main_menu_settings=0x7f080016;
-        public static final int main_tab_comments=0x7f080010;
-        public static final int main_tab_news=0x7f08000e;
-        public static final int main_tab_queue=0x7f08000f;
-        public static final int meneo_item_open=0x7f080012;
-        public static final int meneo_item_vote=0x7f080013;
-        public static final int pref_account=0x7f08000b;
-        public static final int pref_account_apikey=0x7f080008;
-        public static final int pref_account_apikey_summary=0x7f080007;
-        public static final int pref_account_user=0x7f08000a;
-        public static final int pref_account_user_summary=0x7f080009;
-        public static final int pref_app=0x7f080003;
-        public static final int pref_app_refreshonlaunch=0x7f080004;
-        public static final int pref_app_refreshonlaunch_summary=0x7f080005;
-        public static final int pref_style=0x7f080002;
-        public static final int pref_style_mainanimation=0x7f080001;
-        public static final int pref_style_mainanimation_summary=0x7f080000;
+        /**  
+    ================================================================
+    UI Elements
+    ================================================================
+    
+ Main Activity 
+         */
+        public static final int app_name=0x7f080000;
+        /**  
+    ================================================================
+    Preference
+    ================================================================
+    
+         */
+        public static final int app_pref_name=0x7f08000b;
+        /**  UI Lsts 
+         */
+        public static final int empty_list=0x7f080005;
+        public static final int hello=0x7f080001;
+        public static final int main_menu_notame=0x7f080009;
+        /**  Menu 
+         */
+        public static final int main_menu_refresh=0x7f080008;
+        public static final int main_menu_settings=0x7f08000a;
+        public static final int main_tab_comments=0x7f080004;
+        /**  Tab Pages 
+         */
+        public static final int main_tab_news=0x7f080002;
+        public static final int main_tab_queue=0x7f080003;
+        public static final int meneo_item_open=0x7f080006;
+        public static final int meneo_item_vote=0x7f080007;
+        /**  Account Settings 
+         */
+        public static final int pref_account=0x7f08000c;
+        public static final int pref_account_apikey=0x7f08000e;
+        public static final int pref_account_apikey_summary=0x7f08000d;
+        public static final int pref_account_user=0x7f080010;
+        public static final int pref_account_user_summary=0x7f08000f;
+        /**  Application settings 
+         */
+        public static final int pref_app=0x7f080017;
+        public static final int pref_app_refreshonlaunch=0x7f080018;
+        public static final int pref_app_refreshonlaunch_summary=0x7f080019;
+        public static final int pref_app_storage=0x7f08001a;
+        public static final int pref_app_storage_internal=0x7f08001d;
+        public static final int pref_app_storage_none=0x7f08001c;
+        public static final int pref_app_storage_sdcard=0x7f08001e;
+        public static final int pref_app_storage_summary=0x7f08001b;
+        /**  Style customizations 
+         */
+        public static final int pref_style=0x7f080011;
+        public static final int pref_style_mainanimation=0x7f080012;
+        public static final int pref_style_mainanimation_fadein=0x7f080015;
+        public static final int pref_style_mainanimation_none=0x7f080014;
+        public static final int pref_style_mainanimation_slidein=0x7f080016;
+        public static final int pref_style_mainanimation_summary=0x7f080013;
     }
     public static final class style {
         public static final int MNM_Prefs_TextView_ListSeparator=0x7f090006;
