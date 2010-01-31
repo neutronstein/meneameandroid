63a63cb1e4605f00f6c40ed941cadfb45367cc18
B.Thax.DCG <B.Thax.DCG@844bf9d6-0849-11df-ae69-07492475f941>
2010-01-31 09:15:29 +0000

    small fixes and warnings

--- src/meneameApp/src/com/dcg/meneame/BaseRSSWorkerThread.java
+++ src/meneameApp/src/com/dcg/meneame/BaseRSSWorkerThread.java
@@ -4,14 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.concurrent.Semaphore;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.util.EntityUtils;
-import android.app.Activity;
+
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -111,7 +109,7 @@ abstract public class BaseRSSWorkerThread extends Thread {
 			
 			HttpResponse response = client.execute(request);
 			
-			// We stopped!
+			// We have stopped!
 			if ( mbStopRequested ) return;
 			
 			// Build message body
