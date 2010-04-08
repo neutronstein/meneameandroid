34c1b4aa0c4d4b796e490d5f879121d43a9f1ad1
B.Thax.DCG <B.Thax.DCG@844bf9d6-0849-11df-ae69-07492475f941>
2010-04-08 13:58:26 +0000

    v0.1.2 submitted!

---
+++ trunk/src/stable/v0.1.2/meneameApp/src/com/dcg/meneame/CommentsActivity.java
@@ -0,0 +1,60 @@
+package com.dcg.meneame;
+
+import android.os.Bundle;
+
+/**
+ * Comments activity
+ * @author Moritz Wundke (b.thax.dcg@gmail.com)
+ */
+public class CommentsActivity extends FeedActivity {
+	
+	static final int LIST_MENU_OPEN = 0;
+	static final int LIST_MENU_VOTE = 1;
+	
+	public CommentsActivity() {
+		super();
+		
+		// Set feed
+		mFeedURL = "http://www.meneame.net/comments_rss2.php";
+		mListAdapterClass = "com.dcg.meneame.CommentsAdapter";
+		mbIsArticleFeed = false;
+	}
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	/**
+	 * Returns the tag this activity will hold in the main TabWidget
+	 * @return String - TabTag
+	 */
+	public String getTabActivityTag() {
+		return "comments_tab";
+	}
+	
+	/**
+	 * String id used for the tab indicator
+	 * @return
+	 */
+	public int getIndicatorStringID() {
+		return R.string.main_tab_comments;
+	}
+	
+	/**
+	 * Returns the tag this activity will hold in the main TabWidget
+	 * @return String - TabTag
+	 */
+	public static String static_getTabActivityTag() {
+		return "comments_tab";
+	}
+	
+	/**
+	 * String id used for the tab indicator
+	 * @return
+	 */
+	public static int static_getIndicatorStringID() {
+		return R.string.main_tab_comments;
+	}
+}
