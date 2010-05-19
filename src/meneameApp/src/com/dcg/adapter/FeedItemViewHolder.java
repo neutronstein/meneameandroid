a64c740b0f3014aa76d27a67039f3577b2c7910e
B.Thax.DCG <B.Thax.DCG@844bf9d6-0849-11df-ae69-07492475f941>
2010-05-19 17:39:07 +0000

    Provider and adapter done, now I need to integrate it into the current implementation!

---
+++ src/meneameApp/src/com/dcg/adapter/FeedItemViewHolder.java
@@ -0,0 +1,15 @@
+package com.dcg.adapter;
+
+import android.widget.TextView;
+
+public class FeedItemViewHolder {
+	TextView title;
+    TextView description;
+    TextView votes;
+    TextView url;
+    TextView category;
+    
+    int link_id;
+    int feedid;
+    String comment_rss;
+}
--- src/meneameApp/src/com/dcg/provider/FeedItemElement.java
+++ src/meneameApp/src/com/dcg/provider/FeedItemElement.java
@@ -12,12 +12,29 @@ public class FeedItemElement implements BaseColumns {
 	
 	/** Table definition */
 	public static final String LINK_ID = "link_id";
-    public static final String FEEDID = "feedId";    
+	public static final int LINK_ID_FIELD = 0;
+	
+    public static final String FEEDID = "feedId";
+    public static final int FEEDID_FIELD = 1;
+    
     public static final String COMMENT_RSS = "commentRss";
+    public static final int COMMENT_RSS_FIELD = 2;
+    
     public static final String TITLE = "title";
+    public static final int TITLE_FIELD = 3;
+    
     public static final String VOTES = "votes";
+    public static final int VOTES_FIELD = 4;
+    
     public static final String LINK = "link";
+    public static final int LINK_FIELD = 5;
+    
     public static final String DESCRIPTION = "description";
+    public static final int DESCRIPTION_FIELD = 6;
+    
     public static final String CATEGORY = "category";
+    public static final int CATEGORY_FIELD = 7;
+    
     public static final String URL = "url";
+    public static final int URL_FIELD = 8;
 }
